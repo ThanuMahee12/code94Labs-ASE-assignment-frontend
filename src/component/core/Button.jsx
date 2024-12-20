1f5f9d531485fd6ef12d5ba84f41805e0caaf491
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-20 09:00:26 +0530

    refactor(search):Modification Search Bar and Button with disabled option

--- src/component/core/Button.jsx
+++ src/component/core/Button.jsx
@@ -1,14 +1,14 @@
 import React, { memo } from 'react'
 import { Link } from 'react-router-dom'
 
-function Button({ children, onClick, to, className = '', varient = 'normal', rounded = 'normal' }) {
+function Button({ children, onClick, to,others, className = '', varient = 'normal', rounded = 'normal' }) {
 
-    const variantclass = varient === 'normal' ? "bg-[#001EB9] hover:bg-blue-600 text-white" : "border border-2 text-[#001EB9] border-[#001EB9] hover:shadow-md"
+    const variantclass = varient === 'normal' ? "bg-[#001EB9] disabled:bg-sky-700 hover:bg-blue-700 text-white" : "border border-2 text-[#001EB9] border-[#001EB9] disabled:border-blue-700 hover:shadow-md"
     const sizeclass = "px-6 py-2"
     const roundedclass = rounded === 'full' ? 'rounded-full' : 'rounded-lg'
     return to ? 
-    (<Link className={`${className} ${roundedclass} ${sizeclass} ${variantclass} font-semibold`} to={to}>{children}</Link>) : 
-    (<button className={`${className} ${roundedclass} ${sizeclass} ${variantclass} font-semibold`} onClick={onClick}>
+    (<Link {...others} className={`${className} ${roundedclass} ${sizeclass} ${variantclass} font-semibold`} to={to}>{children}</Link>) : 
+    (<button {...others} className={`${className} ${roundedclass} ${sizeclass} ${variantclass} font-semibold `} onClick={onClick}>
         {children}
     </button>)
 }
--- src/component/layout/SearchBar.jsx
+++ src/component/layout/SearchBar.jsx
@@ -1,11 +1,18 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import Button from '../core/Button'
 import {FaSearch} from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 function SearchBar({className}) {
+    const [searchvalue,setsearchValue]=useState('')
+    const navigate=useNavigate()
+    const onChange=e=>setsearchValue(e.target.value)
+const onclick=()=>{
+    navigate(`/search`)
+}
     return (
         <div className={`flex rounded-full bg-gray-100 border border-gray-400 p-2 items-center ${className}`}>
-<input placeholder='search for products' className='w-4/5 bg-transparent outline-none border-none block px-2 placeholder:font-semibold ms-7'></input>
-<Button className='flex items-center gap-x-4' rounded='full'>Search <FaSearch/> </Button>
+<input onChange={onChange} value={searchvalue} placeholder='search for products' className='w-4/5 bg-transparent outline-none border-none block px-2 placeholder:font-semibold ms-7'></input>
+<Button onClick={onclick} disabled={searchvalue==''} className='flex items-center gap-x-4' rounded='full'>Search <FaSearch/> </Button>
         </div>
     )
 }
