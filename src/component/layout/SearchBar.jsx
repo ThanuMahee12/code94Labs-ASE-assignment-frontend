a15fb8f055ed40133bcdea36aeb66e4d3194c00a
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-20 08:42:22 +0530

    refactor(SearchBar):style Searchbar components

--- src/component/layout/SearchBar.jsx
+++ src/component/layout/SearchBar.jsx
@@ -3,9 +3,9 @@ import Button from '../core/Button'
 import {FaSearch} from 'react-icons/fa'
 function SearchBar({className}) {
     return (
-        <div className={`inline-flex  ${className}`}>
-<input className='w-4/5'></input>
-<Button>Search <FaSearch/> </Button>
+        <div className={`flex rounded-full bg-gray-100 border border-gray-400 p-2 items-center ${className}`}>
+<input placeholder='search for products' className='w-4/5 bg-transparent outline-none border-none block px-2 placeholder:font-semibold ms-7'></input>
+<Button className='flex items-center gap-x-4' rounded='full'>Search <FaSearch/> </Button>
         </div>
     )
 }
--- src/layout/SubHeader.jsx
+++ src/layout/SubHeader.jsx
@@ -9,9 +9,9 @@ function SubHeader() {
         <div>
         <h1 className='text-3xl font-semibold'>PRODUCTS</h1>
        </div>
-       <div className='flex items-center justify-between w-full'>
-<SearchBar/> 
-<div className='flex gap-x-8'>
+       <div className='flex items-center justify-between w-full mt-6'>
+<SearchBar className='w-3/5'/> 
+<div className='flex gap-x-8 justify-end w-2/3'>
 <Button to='/new'>New Product</Button>
 <Button to='/favourite' varient='outline'><FaStar size={22}/></Button>
 </div>
